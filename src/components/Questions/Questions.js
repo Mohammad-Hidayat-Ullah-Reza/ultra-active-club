c6a74b09cf1d464e563c1b1f38c7253eb264c14d
mohammadHidayat <mohammadhidayat8@gmail.com>
2022-09-29 19:59:41 +0600

    aside section made sticky and questions component created

---
+++ src/components/Questions/Questions.js
@@ -0,0 +1,23 @@
+import "./Questions.css";
+import React from "react";
+
+const Questions = () => {
+  return (
+    <div>
+      <div>
+        <h3>How does react work?</h3>
+        <p></p>
+      </div>
+      <div>
+        <h3>props vs state?</h3>
+        <p></p>
+      </div>
+      <div>
+        <h3>what useEffect is used for?</h3>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
--- src/components/TimeSection/TimeSection.js
+++ src/components/TimeSection/TimeSection.js
@@ -21,53 +21,53 @@ const TimeSection = ({ times, handleBreakTime, breakTime }) => {
     });
   return (
     <div className="aside-section">
-      <div className="profile-container">
+      <div className="aside-section-subcontainer">
+        <div className="profile-container">
+          <div>
+            <img
+              className="profile-pic"
+              src="https://avatars.githubusercontent.com/u/91150098?v=4"
+              alt="profle pic of Mohammad Hidayat"
+            />
+          </div>
+          <div className="profile-info">
+            <h5 className="profile-name">Mohammad Hidayat</h5>
+            <p className="profession">aspiring frontend web developer</p>
+          </div>
+        </div>
         <div>
-          <img
-            className="profile-pic"
-            src="https://avatars.githubusercontent.com/u/91150098?v=4"
-            alt="profle pic of Mohammad Hidayat"
+          <h4 className="select-break-time">Add A Break</h4>
+          <div className="break-time-option-container">
+            <div onClick={() => handleBreakTime("05")}>05s</div>
+            <div onClick={() => handleBreakTime("10")}>10s</div>
+            <div onClick={() => handleBreakTime("20")}>20s</div>
+            <div onClick={() => handleBreakTime("40")}>40s</div>
+            <div onClick={() => handleBreakTime("50")}>50s</div>
+          </div>
+        </div>
+        <h4 className="exercise-details">Exercise Details</h4>
+        <p className="exercise-time">
+          Exercise Time: <span>{totalTime} sec</span>
+        </p>
+        <p className="break-time">
+          Break Time: <span>{breakTime} sec</span>
+        </p>
+        <div>
+          <button onClick={notify} className="activity-completed-btn">
+            Activity Completed
+          </button>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
           />
         </div>
-        <div className="profile-info">
-          <h5 className="profile-name">Mohammad Hidayat</h5>
-          <p className="profession">aspiring frontend web developer</p>
-        </div>
-      </div>
-      <div>
-        <h4 className="select-break-time">Add A Break</h4>
-        <div className="break-time-option-container">
-          <div onClick={() => handleBreakTime("05")}>05s</div>
-          <div onClick={() => handleBreakTime("10")}>10s</div>
-          <div onClick={() => handleBreakTime("20")}>20s</div>
-          <div onClick={() => handleBreakTime("40")}>40s</div>
-          <div onClick={() => handleBreakTime("50")}>50s</div>
-        </div>
-      </div>
-
-      <h4 className="exercise-details">Exercise Details</h4>
-
-      <p className="exercise-time">
-        Exercise Time: <span>{totalTime} sec</span>
-      </p>
-      <p className="break-time">
-        Break Time: <span>{breakTime} sec</span>
-      </p>
-      <div>
-        <button onClick={notify} className="activity-completed-btn">
-          Activity Completed
-        </button>
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
       </div>
     </div>
   );
